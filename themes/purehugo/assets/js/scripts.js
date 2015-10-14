2e0ea23c25555daa4519df64a65754eda39b9bfd
Psyrax <psyrax@opiumgarden.org>
2015-10-13 22:13:29 -0500

    improved stream

--- themes/purehugo/assets/js/scripts.js
+++ themes/purehugo/assets/js/scripts.js
@@ -1,13 +1,16 @@
 $(function(){
     $('.prettySocial').prettySocial();
     var loadedChan = false;
-    var checkForChannels = 'fritangatv,jimrsng,thewiredfixer,pokemex,UsagiHimura,theattack';
+    var currentChannel = "";
+    var onlineChannels;
     function checkChannels(){
     	$.ajax({
         	url: ' https://api.twitch.tv/kraken/streams?',
-        	data: { 'channel' : checkForChannels, 'client_id' : 'q3zej6c9hamkv74ft8waf9ft9l8lt10'},
+        	data: { 'channel' : 'fritangatv,jimrsng,thewiredfixer,pokemex,UsagiHimura,oso96_2000,leolambertini'},
         	dataType: 'jsonp',
         	success: function(data){
+                onlineChannels = data.streams;
+                console.log(data)
         		if ( data.streams.length > 0 ){
     
         			var selectedChannel = data.streams[0].channel;
@@ -16,9 +19,11 @@ $(function(){
         				insertPlayer(selectedChannel);
         				loadedChan = true;
         			};
-    
+                    $('.statusCircle').removeClass('onlineCircle')
         			$.each(data.streams, function(index, stream){
-    
+                        $('#status-' + stream.channel.name + ' > i').addClass('onlineCircle');
+                        $('#status-' + stream.channel.name).data('live', 'yes');
+                        $('#status-' + stream.channel.name).data('channelindex', index);
         			});
     
         		};
@@ -30,13 +35,25 @@ $(function(){
 	};
     function insertPlayer(channel){
     	var selectedChannel = channel.name;
+        currentChannel = selectedChannel;
     	var displayName = channel.display_name;
     	var gameName = channel.game;
-    	var template = '<h2>'+ displayName +' jugando: '+ gameName +'</h2><div class="embed video-player videoWrapper"><iframe height="378" width="100%" frameborder="0" scrolling="no" src="http://www.twitch.tv/' + selectedChannel + '/embed"></iframe></div>';
+    	var template = '<h2><a href="http://twitch.tv/'+ selectedChannel +'/profile">'+ displayName +'</a> jugando: '+ gameName +'</h2><div class="embed video-player videoWrapper"><iframe height="378" width="100%" frameborder="0" scrolling="no" src="http://www.twitch.tv/' + selectedChannel + '/embed"></iframe></div>';
     	$('#streamZone').empty().append(template);
+        
     };
     checkChannels();
     setInterval(function(){
     	checkChannels();
     }, 300000);
+    $('.streamList').on('click', function(event){
+        if ( $(this).data('live') == "yes" ){
+            event.preventDefault();
+            var index = $(this).data('channelindex');
+            var channel = onlineChannels[index].channel;
+            if ( channel.name != currentChannel ){
+                insertPlayer(channel);
+            };
+        };
+    })
 });
